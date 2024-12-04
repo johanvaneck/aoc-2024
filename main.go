2059cb25880af1c423221f8b69efa2895ecec13f
Johan van Eck <jve.software@gmail.com>
2024-12-05 01:32:42 +0200

    Solved day 4 part 2

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	start := time.Now()
 
-	solution := solutions.Day04Part01()
+	solution := solutions.Day04Part02()
 
 	fmt.Printf("Solution: %d\n", solution)
 
--- solutions/day-04.go
+++ solutions/day-04.go
@@ -29,30 +29,20 @@ func Day04Part01() int {
 	rowsLastIndex := len(matrix) - 1
 	columnsLastIndex := len(matrix[0]) - 1
 
-	diagonalCount := 0
-	colCount := 0
-	rowCount := 0
-
 	for i := range matrix {
 		for j := range matrix[i] {
 			if i <= rowsLastIndex-wordLastIndex && j <= columnsLastIndex-wordLastIndex {
-				fmt.Println(i, j)
 				count += countDiagonal(i, j, matrix, word)
-				diagonalCount += countDiagonal(i, j, matrix, word)
 			}
 			if i <= rowsLastIndex-wordLastIndex {
 				count += countColumn(i, j, matrix, word)
-				colCount += countColumn(i, j, matrix, word)
 			}
 			if j <= columnsLastIndex-wordLastIndex {
 				count += countRow(i, j, matrix, word)
-				rowCount += countRow(i, j, matrix, word)
 			}
 		}
 
 	}
-
-	fmt.Printf("diagonal: %d\ncolumn: %d\nrow: %d\n", diagonalCount, colCount, rowCount)
 
 	return count
 }
@@ -169,4 +159,93 @@ func countColumn(x int, y int, matrix []string, word string) int {
 		}
 	}
 	return count
+}
+
+func Day04Part02() int {
+	file, err := os.Open("inputs/day-04.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	matrix := make([]string, 0)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+		matrix = append(matrix, line)
+	}
+
+	count := 0
+	word := "MAS"
+	wordLastIndex := len(word) - 1
+	rowsLastIndex := len(matrix) - 1
+	columnsLastIndex := len(matrix[0]) - 1
+
+	for i := range matrix {
+		for j := range matrix[i] {
+			if i <= rowsLastIndex-wordLastIndex && j <= columnsLastIndex-wordLastIndex {
+				count += countCrossPairs(i, j, matrix, word)
+			}
+		}
+
+	}
+
+	return count
+}
+
+func countCrossPairs(x int, y int, matrix []string, word string) int {
+	wordLastIndex := len(word) - 1
+	/*
+		M#M
+		#A#
+		S#S
+	*/
+	for i, c := range word {
+		if matrix[x+i][y+i] != byte(c) || matrix[x+wordLastIndex-i][y+i] != byte(c) {
+			break
+		} else if i == wordLastIndex {
+			return 1
+		}
+	}
+	/*
+		S#M
+		#A#
+		S#M
+	*/
+	for i, c := range word {
+		if matrix[x+wordLastIndex-i][y+wordLastIndex-i] != byte(c) || matrix[x+wordLastIndex-i][y+i] != byte(c) {
+			break
+		} else if i == wordLastIndex {
+			return 1
+		}
+	}
+	/*
+		S#S
+		#A#
+		M#M
+	*/
+	for i, c := range word {
+		if matrix[x+wordLastIndex-i][y+wordLastIndex-i] != byte(c) || matrix[x+i][y+wordLastIndex-i] != byte(c) {
+			break
+		} else if i == wordLastIndex {
+			return 1
+		}
+	}
+	/*
+		M#S
+		#A#
+		M#S
+	*/
+	for i, c := range word {
+		if matrix[x+i][y+i] != byte(c) || matrix[x+i][y+wordLastIndex-i] != byte(c) {
+			break
+		} else if i == wordLastIndex {
+			return 1
+		}
+	}
+
+	return 0
 }
