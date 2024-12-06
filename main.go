fd84bca75f04d1984520988b213865bceb006542
Johan van Eck <jve.software@gmail.com>
2024-12-06 10:09:36 +0200

    Day 6 part 2 running, but not correct

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	start := time.Now()
 
-	solution := solutions.Day06Part01()
+	solution := solutions.Day06Part02()
 
 	fmt.Printf("Solution: %d\n", solution)
 
--- solutions/day-06.go
+++ solutions/day-06.go
@@ -62,26 +62,34 @@ func Day06Part01() int {
 
 func move(i int, j int, marker string, matrix [][]rune) (int, int, string) {
 	if marker == "^" {
-		if matrix[i-1][j] == '#' {
+		if i-1 < 0 {
+			return i-1, j, ""
+		} else if matrix[i-1][j] == '#' {
 			return i, j + 1, ">"
 		} else {
 			return i - 1, j, "^"
 		}
 
 	} else if marker == ">" {
-		if matrix[i][j+1] == '#' {
+		if j+1 >= len(matrix[i]) {
+			return i, j + 1, ""
+		} else if matrix[i][j+1] == '#' {
 			return i + 1, j, "v"
 		} else {
 			return i, j + 1, ">"
 		}
 	} else if marker == "<" {
-		if matrix[i][j-1] == '#' {
+		if j-1 < 0 {
+			return i, j-1, ""
+		} else if matrix[i][j-1] == '#' {
 			return i - 1, j, "^"
 		} else {
 			return i, j - 1, "<"
 		}
 	} else if marker == "v" {
-		if matrix[i+1][j] == '#' {
+		if i+1 >= len(matrix) {
+			return i+1, j, ""
+		} else if matrix[i+1][j] == '#' {
 			return i, j - 1, "<"
 		} else {
 			return i + 1, j, "v"
@@ -103,4 +111,74 @@ func findMarker(matrix [][]rune) (int, int, string) {
 		}
 	}
 	return -1, -1, ""
+}
+
+func Day06Part02() int {
+	file, err := os.Open("inputs/day-06.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	originalMatrix := make([][]rune, 0)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		originalMatrix = append(originalMatrix, []rune(line))
+	}
+
+	width := len(originalMatrix)
+	height := len(originalMatrix[0])
+	startI, startJ, startMarker := findMarker(originalMatrix)
+
+	count := 0
+	for i := range originalMatrix {
+		for j := range originalMatrix[i] {
+			// Copy matrix
+			matrix := make([][]rune, len(originalMatrix))
+			for i, line := range originalMatrix {
+				matrix[i] = make([]rune, len(line))
+				copy(matrix[i], line)
+			}
+			// Insert new obstacle
+			matrix[i][j] = '#'
+			if checkLoop(startI, startJ, string(startMarker), matrix, width, height) {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+func checkLoop(startI int, startJ int, startMarker string, matrix [][]rune, width int, height int) bool {
+	i, j := startI, startJ
+	marker := startMarker
+	visited := make([][]string, width)
+	for i := range visited {
+		visited[i] = make([]string, height)
+	}
+
+	for true {
+		fmt.Println(i, j, marker)
+		if i < 0 ||
+			j < 0 ||
+			i >= width-1 ||
+			j >= height-1 ||
+			marker == "" {
+			break
+		}
+
+		if visited[i][j] == marker {
+			return true
+		}
+
+		visited[i][j] = marker
+
+		i, j, marker = move(i, j, marker, matrix[:])
+	}
+
+	return false
 }
